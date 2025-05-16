62f120c79057a20d804632d726dd9891ac9244a9
RitulRai0001 <ritulrai12345@gmail.com>
2025-05-16 13:28:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,65 +1,10 @@
+/**
+ * Detect Electron renderer / nwjs process, which is node, but we should
+ * treat as a browser.
+ */
 
-
-const io = require('socket.io')(8000, {
-    cors: {
-        origin: "http://127.0.0.1:5500",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
-    }
-});
-
-const users = {};
-
-io.on('connection', (socket) => {
-    console.log("A new user connected:", socket.id);
-    
-        socket.emit('welcome-message', ' Welcome to the chat app!');
-    
-
-
-
-    socket.on('new-user-joined', name => {
-        console.log("New user joined:", name);
-        if (Object.values(users).includes(name)) {
-
-            socket.emit('name-already-in-use', name);
-        } else {
-            users[socket.id] = name;
-            socket.emit('existing-users', Object.values(users).filter(user => user !== name));
-
-            socket.broadcast.emit('new-user-joined', name);
-        }
-    });
-
-
-    socket.on('send', message => {
-        // console.log(`Message from ${users[socket.id]}: ${message}`);
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
-    });
-    socket.on('send-file', ({ fileName, fileData }) => {
-        console.log(`${users[socket.id]} sent a file: ${fileName}`);
-        socket.broadcast.emit('receive-file', { fileName, fileData, name: users[socket.id] });
-    });
-    
-
-    socket.on('disconnect', () => {
-        const name = users[socket.id];
-        if (name) {
-            console.log("User disconnected:", name);
-            socket.broadcast.emit('user-left', name);
-            delete users[socket.id];
-        }
-    });
-    socket.on('already-user-joined', () => {
-        console.log("User tried to join with an already used name.");
-    });
-    socket.on('existing-users', () => {
-        console.log("alredy joined the chat  ")
-    })
-
-});
-
-
-
-
+if (typeof process === 'undefined' || process.type === 'renderer' || process.browser === true || process.__nwjs) {
+	module.exports = require('./browser.js');
+} else {
+	module.exports = require('./node.js');
+}
